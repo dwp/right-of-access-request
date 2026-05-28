4177cb78295f350f39402b157c124299eff1f7a2
Rinto Cyriac <rinto.cyriac@engineering.digital.dwp.gov.uk>
2026-05-28 17:23:06 +0100

    Specific details
    GitOrigin-RevId: c207de66e95b200392850f13bc789726d995f46d

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,47 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+//Date filter to cover xx to xx
+addFilter('dateRangeYearsAgo', (_, fromYearsAgo, toYearsAgo) => {
+    const today = new Date();
+
+    const fromDate = new Date(today);
+    fromDate.setFullYear(today.getFullYear() - fromYearsAgo);
+
+    const toDate = new Date(today);
+    toDate.setFullYear(today.getFullYear() - toYearsAgo);
+
+    function format(date) {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        return `${day} ${month} ${year}`;
+    }
+
+    return {
+        from: format(fromDate),
+        to: format(toDate)
+    };
+});
+
+//30 days ago filter
+addFilter('dateMinusDays', (_, daysToSubtract) => {
+    const today = new Date();
+
+    const pastDate = new Date(today);
+    pastDate.setDate(today.getDate() - daysToSubtract);
+
+    const day = pastDate.getDate();
+    const month = pastDate.getMonth() + 1;
+    const year = pastDate.getFullYear();
+
+    return `${day} ${month} ${year}`;
+});
+
+
+
+
+
+
+
+
